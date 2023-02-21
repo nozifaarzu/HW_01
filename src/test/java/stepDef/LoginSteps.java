5acdccafcd1d4c11c083af4436acf4b775d12f6c
nozifaarzu <120534172+nozifaarzu@users.noreply.github.com>
2023-02-20 20:55:07 -0500

    test

--- src/test/java/stepDef/LoginSteps.java
+++ src/test/java/stepDef/LoginSteps.java
@@ -1,6 +1,9 @@
 package stepDef;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class LoginSteps {
     @Given("user in TalentTek homepage")
@@ -8,4 +11,34 @@ public class LoginSteps {
         //verify page title
         //all java, selenium code stays here
     }
+
+
+    @Then("user should be able to log in successfully")
+    public void userShouldBeAbleToLogInSuccessfully() {
+    }
+
+    @And("user enter valid email address")
+    public void userEnterValidEmailAddress() {
+    }
+
+    @And("user enter invalid password")
+    public void userEnterInvalidPassword() {
+    }
+
+    @When("user clicks on Log In button")
+    public void userClicksOnLogInButton() {
+    }
+
+    @Then("user should be able to see {string}")
+    public void userShouldBeAbleToSee(String arg0) {
+    }
+
+    @And("user enter invalid email address")
+    public void userEnterInvalidEmailAddress() {
+    }
+
+    @And("user enter valid password")
+    public void userEnterValidPassword() {
+    }
+
 }
